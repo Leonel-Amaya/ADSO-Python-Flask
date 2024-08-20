7ccd31ec6bf280c87f3aa351df27c24a2a2b40f3
Leonel Amaya <omar.mejia2099@gmail.com>
2024-08-19 19:36:26 -0500

    correccion funciones

--- src/app.py
+++ src/app.py
@@ -10,17 +10,17 @@ def index():
     return render_template('principal.html')
 
 @app.route('/registrar-producto')
-def crear_producto:
+def crear_producto():
     return render_template('producto.html')
 
 @app.route('/registrar-cliente')
-def crear_cliente:
+def crear_cliente():
     return render_template('cliente.html')
 
 @app.route('/registrar-proveedor')
-def crear_proveedor:
+def crear_proveedor():
     return render_template('proveedor.html')
 
 @app.route('/registrar-factura')
-def crear_factura:
+def crear_factura():
     return render_template('facturar.html')
