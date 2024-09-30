852b5bc71e1cd35c93e88cbaedffe5e2b6639766
Leonel Amaya <omar.mejia2099@gmail.com>
2024-09-29 20:45:46 -0500

    Implementando layout

--- src/app.py
+++ src/app.py
@@ -7,20 +7,20 @@ if __name__ == '__main__':
 
 @app.route('/')
 def index():
-    return render_template('principal.html')
+    return render_template('principal.html', title_page = 'SFI - Home')
 
 @app.route('/registrar-producto')
 def crear_producto():
-    return render_template('producto.html')
+    return render_template('producto.html', title_page = 'SFI - Productos')
 
 @app.route('/registrar-cliente')
 def crear_cliente():
-    return render_template('cliente.html')
+    return render_template('cliente.html', title_page = 'SFI - Cliente')
 
 @app.route('/registrar-proveedor')
 def crear_proveedor():
-    return render_template('proveedor.html')
+    return render_template('proveedor.html', title_page = 'SFI - Proveedor')
 
 @app.route('/registrar-factura')
 def crear_factura():
-    return render_template('facturar.html')
+    return render_template('facturar.html', title_page = 'SFI - Factura')
